014afd79d6c60838f4dbe73a2808dfc423872d63
Eric Wehrly <eric@disruptiontheory.com>
2016-01-25 23:55:11 -0500

    Attempting to add infrastructure files for continuous integration.

---
+++ SoManyZombies/Requests/RebuildServer.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using Nancy;
+
+namespace SoManyZombies.Requests
+{
+    public class RebuildServer : NancyModule
+    {
+        public RebuildServer()
+        {
+            Get["/"] = LaunchRebuildScript;
+        }
+
+        private object LaunchRebuildScript(dynamic parameters)
+        {
+            var pr = new Process();
+            var prs = new ProcessStartInfo {FileName = @"../../../server-rebuild.sh"};
+            pr.StartInfo = prs;
+
+            var ths = new ThreadStart(() => pr.Start());
+            new Thread(ths).Start();
+
+            return "Initiating rebuild.";
+        } 
+    }
+}
---
+++ server-rebuild.sh
@@ -0,0 +1,5 @@
+# This file should be triggered from a post message given by a github webhook to the SoManyZombiesServer application
+
+# This file should pull from git, build, verify the build, shut down the server, and start up the new one...
+
+echo "hi" >> out.out
